0d55b982829ce1afd3ee97cbf23b3afceb313f65
Artur Gomes Barreto <artur.gomes.barreto@gmail.com>
2024-11-11 19:57:32 -0300

    Feat: draw_graph showing node colors by class.

--- graph/graph.py
+++ graph/graph.py
@@ -20,8 +20,20 @@ class Graph:
             print(edge)
 
     def draw_graph(self, title="Graph"):
+        # Generate a sorted list of unique class labels
+        unique_classes = sorted(set(self.node_classes))
+
+        # Define colors (ensure there are enough colors for the number of classes)
+        colors = ['orange', 'lightgreen', 'blue', 'pink', 'yellow', 'red']
+
+        # Map each class label to a color
+        class_to_color = {cls: colors[i % len(colors)] for i, cls in enumerate(unique_classes)}
+
+        # Assign colors to nodes based on their classes
+        node_colors = [class_to_color[self.node_classes[node]] for node in self.G.nodes()]
+
         self.pos = nx.spring_layout(self.G, seed=42)
-        nx.draw(self.G, self.pos, with_labels=True, node_color='lightblue',
+        nx.draw(self.G, self.pos, with_labels=True, node_color=node_colors,
                 node_size=500, font_size=10, font_weight='bold')
         labels = nx.get_edge_attributes(self.G, 'weight')
         nx.draw_networkx_edge_labels(self.G, self.pos, edge_labels=labels)
